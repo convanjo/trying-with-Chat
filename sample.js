a499f1836bd152f161c3ab3a96349d821d9c0126
convanjo <85142788+convanjo@users.noreply.github.com>
2023-01-24 23:04:24 +0800

    first try
    so great

---
+++ sample.js
@@ -0,0 +1,11 @@
+document.getElementById("about").addEventListener("click", function(){
+  alert("About Us Clicked!");
+});
+
+document.getElementById("services").addEventListener("click", function(){
+  alert("Services Clicked!");
+});
+
+document.getElementById("contact").addEventListener("click", function(){
+  alert("Contact Us Clicked!");
+});
